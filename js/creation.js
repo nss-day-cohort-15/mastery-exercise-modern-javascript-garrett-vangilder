2f4d21abe20cb6db1352eaba55a0b7d6d59539b0
Garrett Vangilder <garrett.vangilder@gmail.com>
2016-08-28 18:52:55 -0500

    changed default name

--- js/creation.js
+++ js/creation.js
@@ -4,74 +4,74 @@ var RobotDome = (function(robotdome) {
         // GOOD GUY CLASS SELECTION
         $('.SkeletonGG').on('click', ()=> {
             player = new RobotDome.SkeletonBot();
-            player.name = RobotDome.name;
+            player.name = RobotDome.name || "Unknown Traveler";
             $('.toBGClasses').prop('disabled', false);
         });
 
         $('.VampireGG').on('click', ()=> {
             player = new RobotDome.VampireBot();
-            player.name = RobotDome.name;
+            player.name = RobotDome.name || "Unknown Traveler";
             $('.toBGClasses').prop('disabled', false);
         });
 
         $('.SpaceCowboyGG').on('click', ()=> {
             player = new RobotDome.SpaceCowboyBot();
-            player.name = RobotDome.name;
+            player.name = RobotDome.name || "Unknown Traveler";
             $('.toBGClasses').prop('disabled', false);
         });
 
         $('.AstroGG').on('click', ()=> {
             player = new RobotDome.AstroBot();
-            player.name = RobotDome.name;
+            player.name = RobotDome.name || "Unknown Traveler";
             $('.toBGClasses').prop('disabled', false);
         });
 
         $('.RangerGG').on('click', ()=> {
             player = new RobotDome.RangerBot();
-            player.name = RobotDome.name;
+            player.name = RobotDome.name || "Unknown Traveler";
             $('.toBGClasses').prop('disabled', false);
         });
 
         $('.RodeoGG').on('click', ()=> {
             player = new RobotDome.RodeoBot();
-            player.name = RobotDome.name;
+            player.name = RobotDome.name || "Unknown Traveler";
             $('.toBGClasses').prop('disabled', false);
         });
 
         // BAD GUY CLASS SELECTION
         $('.SkeletonBG').on('click', ()=> {
             badGuy = new RobotDome.SkeletonBot();
-            badGuy.name = RobotDome.nameBG;
+            badGuy.name = RobotDome.nameBG || "Evil Bot";
             $('.toBattleGround').prop('disabled', false);
         });
 
         $('.VampireBG').on('click', ()=> {
             badGuy = new RobotDome.VampireBot();
-            badGuy.name = RobotDome.nameBG;
+            badGuy.name = RobotDome.nameBG || "Evil Bot";
             $('.toBattleGround').prop('disabled', false);
         });
 
         $('.SpaceCowboyBG').on('click', ()=> {
             badGuy = new RobotDome.SpaceCowboyBot();
-            badGuy.name = RobotDome.nameBG;
+            badGuy.name = RobotDome.nameBG || "Evil Bot";
             $('.toBattleGround').prop('disabled', false);
         });
 
         $('.AstroBG').on('click', ()=> {
             badGuy = new RobotDome.AstroBot();
-            badGuy.name = RobotDome.nameBG;
+            badGuy.name = RobotDome.nameBG || "Evil Bot";
             $('.toBattleGround').prop('disabled', false);
         });
 
         $('.RangerBG').on('click', ()=> {
             badGuy = new RobotDome.RangerBot();
-            badGuy.name = RobotDome.nameBG;
+            badGuy.name = RobotDome.nameBG || "Evil Bot";
             $('.toBattleGround').prop('disabled', false);
         });
 
         $('.RodeoBG').on('click', ()=> {
             badGuy = new RobotDome.RodeoBot();
-            badGuy.name = RobotDome.nameBG;
+            badGuy.name = RobotDome.nameBG || "Evil Bot";
             $('.toBattleGround').prop('disabled', false);
         });
     });
